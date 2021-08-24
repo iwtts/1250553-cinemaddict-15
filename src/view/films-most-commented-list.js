af932c997e6b180d9f7e97b245252d14fe914caf
iwtts <berzinao@hotmail.com>
2021-08-24 23:50:53 +0300

    рефакторинг модулей дополнительных списков

--- src/view/films-most-commented-list.js
+++ src/view/films-most-commented-list.js
@@ -3,10 +3,6 @@ import AbstractView from './abstract.js';
 const createFilmsMostCommentedListTemplate = () => (
   `<section class="films-list films-list--extra">
     <h2 class="films-list__title">Most commented</h2>
-
-    <div class="films-list__container">
-
-    </div>
   </section>`
 );
 
