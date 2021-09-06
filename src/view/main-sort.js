19bb26d6e9f1e620361814c0f358bf43f1b0abfe
iwtts <berzinao@hotmail.com>
2021-09-06 16:29:26 +0300

    доработка шаблона сортировки

--- src/view/main-sort.js
+++ src/view/main-sort.js
@@ -1,10 +1,11 @@
 import AbstractView from './abstract';
+import { SortType } from '../const';
 
 const createMainSortTemplate = () => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
+    <li><a href="#" class="sort__button sort__button--active data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button data-sort-type="${SortType.DATE}">Sort by date</a></li>
+    <li><a href="#" class="sort__button data-sort-type="${SortType.RATING}">Sort by rating</a></li>
   </ul>`
 );
 
