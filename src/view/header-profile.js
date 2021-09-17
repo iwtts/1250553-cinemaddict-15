1499596ae9b085a7f551232825164a4ba54eaded
iwtts <berzinao@hotmail.com>
2021-09-17 10:39:43 +0300

    реализация отображения звания в шапке

--- src/view/header-profile.js
+++ src/view/header-profile.js
@@ -1,14 +1,20 @@
 import AbstractView from './abstract';
+import { getWatchedFilms, getRank } from '../utils/stats';
 
-const createHeaderProfileTemplate = () => (
+const createHeaderProfileTemplate = (films) => (
   `<section class="header__profile profile">
-    <p class="profile__rating">Movie Buff</p>
+    <p class="profile__rating">${getRank(getWatchedFilms(films).length)}</p>
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
   </section>`
 );
 
 export default class HeaderProfile extends AbstractView {
+  constructor(films) {
+    super();
+    this._films = films;
+  }
+
   getTemplate() {
-    return createHeaderProfileTemplate();
+    return createHeaderProfileTemplate(this._films);
   }
 }
