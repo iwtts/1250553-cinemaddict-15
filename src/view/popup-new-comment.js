dbd771980fcdb248766dbf3da01aeea4040704dc
iwtts <berzinao@hotmail.com>
2021-09-24 10:14:36 +0300

    исправление переключения эмоции нового коммента

--- src/view/popup-new-comment.js
+++ src/view/popup-new-comment.js
@@ -1,14 +1,14 @@
 import SmartView from './smart';
 
 const createPopupNewCommentTemplate = (data) => {
-  const { checkedEmotion } = data;
+  const { checkedEmotion, text } = data;
 
   return `<div class="film-details__new-comment">
             <div class="film-details__add-emoji-label">
               ${checkedEmotion ? `<img src="images/emoji/${checkedEmotion}.png" width="55" height="55" alt="emoji-${checkedEmotion}">` : ''}
             </div>
             <label class="film-details__comment-label">
-              <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
+              <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment">${text ? text : ''}</textarea>
             </label>
             <div class="film-details__emoji-list">
               <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-smile" value="smile">
@@ -36,6 +36,7 @@ export default class PopupNewComment extends SmartView {
     super();
     this._data = PopupNewComment.parseToData();
 
+    this._commentInputHandler = this._commentInputHandler.bind(this);
     this._emotionChangeHandler = this._emotionChangeHandler.bind(this);
     this._addCommentHandler = this._addCommentHandler.bind(this);
     this._checkedEmotion = null;
@@ -56,6 +57,9 @@ export default class PopupNewComment extends SmartView {
     this.getElement()
       .querySelector('.film-details__emoji-list')
       .addEventListener('change', this._emotionChangeHandler);
+    this.getElement()
+      .querySelector('.film-details__comment-input')
+      .addEventListener('input', this._commentInputHandler);
   }
 
   _onEmotionChange(emotion) {
@@ -68,6 +72,13 @@ export default class PopupNewComment extends SmartView {
     if (evt.target.matches('input[type="radio"]')) {
       this._onEmotionChange(evt.target.value);
     }
+  }
+
+  _commentInputHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      text: evt.target.value,
+    }, true);
   }
 
   _addCommentHandler(evt) {
@@ -94,6 +105,7 @@ export default class PopupNewComment extends SmartView {
   static parseToData() {
     return Object.assign(
       {
+        text: null,
         checkedEmotion: null,
         isDisabled: false,
       },
--- src/view/smart.js
+++ src/view/smart.js
@@ -6,7 +6,7 @@ export default class Smart extends AbstractView {
     this._data = {};
   }
 
-  updateData(update) {
+  updateData(update, justDataUpdating) {
     if (!update) {
       return;
     }
@@ -16,6 +16,10 @@ export default class Smart extends AbstractView {
       this._data,
       update,
     );
+
+    if (justDataUpdating) {
+      return;
+    }
 
     this.updateElement();
   }
